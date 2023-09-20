aa4a0e343fbd95c3994eb5aa993a66745d0ed9ce
Maxime Clavel <maximeclavel34@gmail.com>
2023-09-20 03:35:05 -0400

    config sfdx

---
+++ .sfdx/typings/lwc/X04370bb7cc12c33bd5fdbb6c6a22c6cfque.asset.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/contentAssetUrl/X04370bb7cc12c33bd5fdbb6c6a22c6cfque" {
+    var X04370bb7cc12c33bd5fdbb6c6a22c6cfque: string;
+    export default X04370bb7cc12c33bd5fdbb6c6a22c6cfque;
+}
